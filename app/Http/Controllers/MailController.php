214c230387f380df71cb3140ea65281e6c645016
unknown <danielmarista@gmail.com>
2020-05-18 10:52:45 -0300

    Correction about sendMail by AWSSES

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+
+use App\Mail\ContactMail;
+ 
+class MailController extends Controller
+{
+
+    public function send(Request $request, $layout)
+    {
+        switch($layout){
+            case "contact-mail-site":
+                $r = $this->sendMailFromHotSite($request->all());
+            break;   
+        }
+
+        return $r;
+    }
+
+    /**
+     * @return Response
+     */
+    private function sendMailFromHotSite($data){
+    
+        Mail::to($data['email'])->send(new ContactMail($data));
+        
+        if(count(Mail::failures()) > 0){
+            return response()->json(['success'=>true, 'msg'=>'Failed to send'], 500);
+        }else{
+            return response()->json(['success'=>true, 'msg'=>'Mail sent!']);
+        }
+        
+    }
+
+
+}
